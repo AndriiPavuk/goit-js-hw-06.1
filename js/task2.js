bf8489ca45c224c2619e5e83c846e57ebb6adf44
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2023-11-19 19:43:42 +0200

    1

---
+++ js/task2.js
@@ -0,0 +1,47 @@
+class Storage {
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
+    }
+  }
+}
+
+// class storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+
+//   getitems() {
+//     return this.items;
+//   }
+
+//   additem(newitem) {
+//     this.items.push(newitem);
+//   }
+
+//   removeitem(itemtoremove) {
+//     this.items = this.items.filter(item => item !== itemtoremove);
+//   }
+// }
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
